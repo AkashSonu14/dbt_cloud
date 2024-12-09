6a11d8dc687dca9650739eb818261a6664a80cd0
Akash A <akasha14122001@gmail.com>
2024-12-09 05:06:11 +0000

    commit

---
+++ macros/join.sql
@@ -0,0 +1,19 @@
+{% macro join_macro(status, gender) %}
+    {% set query %}
+        SELECT 
+            CustomerKey,
+            FirstName,
+            LastName,
+            EmailAddress,
+            YearlyIncome,
+            MaritalStatus,
+            Gender
+        FROM dimcustomer
+        LEFT JOIN dimgeography
+            ON dimcustomer.geographykey = dimgeography.geographykey
+        WHERE maritalstatus = '{{ status }}' 
+          AND gender = '{{ gender }}'
+    {% endset %}
+
+    {{ return(query) }}
+{% endmacro %}
